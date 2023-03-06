76ec0e35ee9263ea45c4e19fe52f25ea2f053241
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-06 18:37:14 -0500

    added my name

--- src/main/java/com/github/zipcodewilmington/sample/Person.java
+++ src/main/java/com/github/zipcodewilmington/sample/Person.java
@@ -1,4 +1,6 @@
 package com.github.zipcodewilmington.sample;
-
+/*
+James Jang
+ */
 public class Person {
 }
--- src/test/java/com/github/zipcodewilmington/sample/PersonTest.java
+++ src/test/java/com/github/zipcodewilmington/sample/PersonTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 /**
  * @author git-leon
  * @version 1.0.0
